2f1931baf676e7218155e14e86151818836fbefa
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-06-28 16:10:11 -0400

    ex1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 
 # Update with the exercise you are trying to test
 from src import test
+from src import solutions
 
 
 def main():
     test.hello()
+
+    solutions.sort_people(solutions.people_list, 'age', 'asc')
+    print(solutions.people_list)
+
 
 
 if __name__ == '__main__':
--- src/solutions.py
+++ src/solutions.py
@@ -1,4 +1,26 @@
 from pprint import pprint
 
-def ex1():
-    pass #TODO:
+
+def sort_people(list_people, sort_by, asc_desc):
+    if asc_desc == 'asc':
+        list_people.sort(key=lambda p: p[sort_by])
+    else:
+        list_people.sort(key=lambda p: p[sort_by], reverse=True)
+
+
+people_list = [
+    {'name': 'alice',   'age': 20, 'weight': 160, 'sex': 'male',   'id': 1},
+    {'name': 'bob',     'age': 10, 'weight': 130, 'sex': 'male',   'id': 2},
+    {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+]
+sort_people(people_list, 'age', 'asc')
+print(people_list)
+
+
+def filter_males(list_people):
+    only_males = list(filter(lambda p: p['sex'] == 'male', people_list))
+    return only_males
+
+
+filtered_list = filter_males(people_list)
+print()
